954effddfc372bbb02ff03861ec867502fcb13bd
danielperez9 <108952044+danielperez9@users.noreply.github.com>
2022-08-19 14:59:22 -0400

    Update andom.py

--- andom.py
+++ andom.py
@@ -5,7 +5,7 @@ def factorial(n):
     return 1 if (n==1 or n==0) else n * factorial(n - 1);
     
  
-# Driver Code
+# code to start factorial
 num =10;
 print("Factorial of",num,"is",factorial(num))
 print('Hello world')
